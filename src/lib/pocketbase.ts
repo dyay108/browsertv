55972ebedc36b87b9e3989acce1b9c71cef26498
dyay108 <17689521+dyay108@users.noreply.github.com>
2025-03-28 13:45:02 -0500

    switched to vite

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -1,7 +1,7 @@
 import PocketBase, { AuthModel } from 'pocketbase';
 
 // Initialize the PocketBase client
-export const pb = new PocketBase(process.env.POCKETBASE_URL || 'http://127.0.0.1:8090');
+export const pb = new PocketBase(import.meta.env.VITE_POCKETBASE_URL || 'http://localhost:8090'); 
 
 // Export current user as a convenience
 export const getCurrentUser = (): AuthModel | null => pb.authStore.model;
